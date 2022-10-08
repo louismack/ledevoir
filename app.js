e7bec81d8dd519f26cccf05c5d1e16c884f7bd33
lm <lm@hash.fyi>
2022-10-07 20:02:50 -0400

    initial add

---
+++ app.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+import jsdom from "jsdom";
+import http from "http";
+const BASE_DOMAIN = "https://www.ledevoir.com";
+
+const requestListener = async function (req, res) {
+  const url = new URL(req.url, "https://mydomain.com/");
+  const start = url.searchParams.get("start") ?? 1;
+  const end = url.searchParams.get("end") ?? 20;
+  const response = await axios.get(BASE_DOMAIN);
+  const htmlDoc = new jsdom.JSDOM(response.data, "text/html");
+  const newHtmlDoc = new jsdom.JSDOM(response.data, "text/html");
+  newHtmlDoc.window.document
+    .querySelector("head")
+    .insertAdjacentHTML("afterbegin", `<base href="${BASE_DOMAIN + "/"}">`);
+  while (newHtmlDoc.window.document.body.firstChild) {
+    newHtmlDoc.window.document.body.firstChild.remove();
+  }
+  const links = htmlDoc.window.document.querySelectorAll(".card-click");
+  for (let i = start * 1 - 1; i < links.length && i < end * 1; i++) {
+    const link = links[i];
+    let url = link.href.includes(BASE_DOMAIN)
+      ? link.href
+      : BASE_DOMAIN + link.href;
+    const res = await axios.get(url);
+    const doc = new jsdom.JSDOM(
+      "<div style='width: 100vw;'>" + res.data + "</div>",
+      "text/html"
+    );
+    //   link.parent.parent.parent.insertAdjacentElement(
+    //     "afterend",
+    //     doc.window.document.body
+    //   );
+    doc.window.document.querySelector("header").remove();
+    doc.window.document.querySelector("footer").remove();
+    newHtmlDoc.window.document.body.insertBefore(
+      doc.window.document.body,
+      null
+    );
+  }
+  res.writeHead(200);
+  res.end(newHtmlDoc.serialize());
+};
+
+const server = http.createServer(requestListener);
+server.listen(8080);
+server.on("listening", () => console.log("listening"));
